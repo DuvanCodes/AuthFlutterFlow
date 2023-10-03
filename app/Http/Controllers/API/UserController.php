c10845a59969baa11899e7bb081051e44f379640
DuvanCodes <duvancc2901@gmail.com>
2023-10-03 10:28:49 -0500

    first commit

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Auth;
+
+class UserController extends Controller
+{
+    public function loginUser(Request $request): Response
+    {
+        $input = $request->all();
+        Auth::attempt($input);
+
+        $user = Auth::user();
+
+        $token = $user->createToken('example')->accessToken;
+        return response()->json(['status' => 200, 'token' => $token], 200);
+    }
+
+    public function getUserDetail(): Response
+    {
+        if(Auth::guard('api')->check()){
+            $user = Auth::guard('api')->user();
+            return Response(['data' => $user], 200);
+        }
+        return Response(['data' => 'Unauthorized'], 401);
+    }
+
+    public function getuserLogout()
+    {
+        if(Auth::guard('api')->check()){
+            $accessToken = Auth::guard('api')->user()->token();
+
+                \DB::table('oauth_refresh_tokens')
+                    ->where('access_token_id', $accessToken->id)
+                    ->update(['revoked' => true]);
+            $accessToken->revoke();
+
+            return Response(['data' => 'Unauthorized','message' => 'User logout successfully.'],200);
+        }
+        return Response(['data' => 'Unauthorized'],401);
+    }
+
+}
